764203a8260517723ea9dd74abd6a79f905fada5
Mohamed-Gouse <gouasgauz@gmail.com>
2023-12-14 10:40:31 +0530

    tenthpush

--- PrinterWorld/settings.py
+++ PrinterWorld/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'paypal.standard.ipn'
 ]
 
+SITE_ID = 1
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
