5eb55e33ceeba0607272b7c59e24a3cab75fa2ba
Mohamed-Gouse <gouasgauz@gmail.com>
2023-12-14 11:49:18 +0530

    eleventhpush

--- PrinterWorld/settings.py
+++ PrinterWorld/settings.py
@@ -10,7 +10,7 @@ DEBUG = True
 
 INTERNAL_IPS = ('13.53.198.120',)
 
-CSRF_TRUSTED_ORIGINS = ['https://jb-computers.shop', '13.53.198.120']
+CSRF_TRUSTED_ORIGINS = ['https://jb-computers.shop', 'http://13.53.198.120']
 
 ALLOWED_HOSTS = ['127.0.0.1', '13.53.198.120', 'jb-computers.shop']
 
